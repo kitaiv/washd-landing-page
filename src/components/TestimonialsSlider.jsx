4d1729e9c23e4335e1544528d522c83f055f003c
kitaiv <43699040+kitaiv@users.noreply.github.com>
2022-04-23 13:54:04 +0300

    testimonials slides for mobile fix

--- src/components/TestimonialsSlider.jsx
+++ src/components/TestimonialsSlider.jsx
@@ -1,128 +1,113 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import classnames from "classnames";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import s from '../assets/style/style.module.css'
-import "swiper/css/effect-coverflow";
+import s from "../assets/style/style.module.css";
+import "swiper/css/effect-cards";
 
-import userpic1 from '../assets/images/userpic-1.svg'
-import userpic2 from '../assets/images/userpic-2.svg'
-import userpic3 from '../assets/images/userpic-3.svg'
-import leftbtnIcon from '../assets/images/left-btn-icon.svg'
-import rightbtnIcon from '../assets/images/right-btn-icon.svg'
+import userpic1 from "../assets/images/userpic-1.svg";
+import userpic2 from "../assets/images/userpic-2.svg";
+import userpic3 from "../assets/images/userpic-3.svg";
+import leftbtnIcon from "../assets/images/left-btn-icon.svg";
+import rightbtnIcon from "../assets/images/right-btn-icon.svg";
 
-import { Navigation, EffectCoverflow, Pagination } from "swiper";
-import {Breakpoint} from "react-socks";
+import { Navigation, EffectCards } from "swiper";
+import { Breakpoint } from "react-socks";
 
 export default function TestimonialsSlider() {
-    const testimonials = [
-        {
-            icon: userpic3,
-            name: 'Rodolfo Goode',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.'
-        },
-        {
-            icon: userpic2,
-            name: 'Mike Asskicker',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.'
-        },
-        {
-            icon: userpic3,
-            name: 'Clinton Mcclure',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.'
-        },
-        {
-            icon: userpic1,
-            name: 'Clinton Mcclure',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.'
-        },
-        {
-            icon: userpic3,
-            name: 'Clinton Mcclure',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.'
-        },
-        {
-            icon: userpic2,
-            name: 'Clinton Mcclure',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.'
-        }
-    ]
+  const testimonials = [
+    {
+      icon: userpic3,
+      name: "Rodolfo Goode",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.",
+    },
+    {
+      icon: userpic2,
+      name: "Mike Asskicker",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.",
+    },
+    {
+      icon: userpic3,
+      name: "Clinton Mcclure",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.",
+    },
+    {
+      icon: userpic1,
+      name: "Clinton Mcclure",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.",
+    },
+    {
+      icon: userpic3,
+      name: "Clinton Mcclure",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.",
+    },
+    {
+      icon: userpic2,
+      name: "Clinton Mcclure",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac feugiat adipiscing eu augue commodo etiam lacus egestas blandit.",
+    },
+  ];
 
-    return(
-        <>
-            <Breakpoint customQuery="(min-width: 768px)">
-                <div className={s.sliderWrapper}>
-                    <Swiper
-                        navigation={true}
-                        slidesPerView={3}
-                        spaceBetween={2}
-                        centeredSlides={true}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        className="mySwiper"
-                        modules={[Navigation]}
-                        loop={true}
-                    >
-                        {
-                            testimonials.map((el, key) => (
-                                <SwiperSlide key={key}>
-                                    <div className={classnames(s.testimonialsCard)}>
-                                        <div className={s.testimonialHeader}>
-                                            <img src={el.icon} alt="avatar"/>
-                                            <h3>{el.name}</h3>
-                                        </div>
-                                        <div>
-                                            <h4>{el.text}</h4>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            ))
-                        }
-                    </Swiper>
+  return (
+    <>
+      <Breakpoint customQuery="(min-width: 768px)">
+        <div className={s.sliderWrapper}>
+          <Swiper
+            navigation={true}
+            slidesPerView={3}
+            spaceBetween={2}
+            centeredSlides={true}
+            pagination={{
+              clickable: true,
+            }}
+            className="mySwiper"
+            modules={[Navigation]}
+            loop={true}
+          >
+            {testimonials.map((el, key) => (
+              <SwiperSlide key={key}>
+                <div className={classnames(s.testimonialsCard)}>
+                  <div className={s.testimonialHeader}>
+                    <img src={el.icon} alt="avatar" />
+                    <h3>{el.name}</h3>
+                  </div>
+                  <div>
+                    <h4>{el.text}</h4>
+                  </div>
                 </div>
-            </Breakpoint>
-            <Breakpoint small down>
-                <div className={s.sliderWrapper}>
-                    <Swiper
-                        effect={"coverflow"}
-                        grabCursor={true}
-                        centeredSlides={true}
-                        slidesPerView={"auto"}
-                        coverflowEffect={{
-                            rotate: 50,
-                            stretch: 0,
-                            depth: 100,
-                            modifier: 1,
-                            slideShadows: true,
-                        }}
-                        modules={[EffectCoverflow, Pagination]}
-                        className="mySwiper"
-                        navigation={true}
-                        pagination={{
-                            clickable: true,
-                        }}
-                        loop={true}
-                    >
-                        {
-                            testimonials.map((el, key) => (
-                                <SwiperSlide key={key}>
-                                    <div className={classnames(s.testimonialsCard)}>
-                                        <div className={s.testimonialHeader}>
-                                            <img src={el.icon} alt="avatar"/>
-                                            <h3>{el.name}</h3>
-                                        </div>
-                                        <div>
-                                            <h4>{el.text}</h4>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                            ))
-                        }
-                    </Swiper>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </Breakpoint>
+      <Breakpoint small down>
+        <div className={s.sliderWrapper}>
+          <Swiper
+            effect={"cards"}
+            grabCursor={true}
+            modules={[EffectCards]}
+            className="mySwiper"
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            // navigation={true}
+            loop={true}
+          >
+            {testimonials.map((el, key) => (
+              <SwiperSlide key={key}>
+                <div className={classnames(s.testimonialsCard)}>
+                  <div className={s.testimonialHeader}>
+                    <img src={el.icon} alt="avatar" />
+                    <h3>{el.name}</h3>
+                  </div>
+                  <div>
+                    <h4>{el.text}</h4>
+                  </div>
                 </div>
-            </Breakpoint>
-        </>
-    )
-
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </Breakpoint>
+    </>
+  );
 }
